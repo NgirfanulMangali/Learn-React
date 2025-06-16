67922bf5df6b1ecca9b4968b0a94a43181764d1d
NgirfanulMangali <metalqwerty0@gmail.com>
2025-06-16 19:36:25 -0400

    change styling css input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-export default function App() {
-  const [title, setTitle] = useState('');
+export default function MyForm() {
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
 
-  useEffect(() => {
-    getData();
-  }, []);
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
 
-  const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const task = await response.json();
-    console.log(task)
-    setTitle(task.title);
-  };
+    // You can pass formData as a fetch body directly:
+    fetch('/some-api', { method: form.method, body: formData });
 
-  return <h1>{title}</h1>;
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Text input: <input name="myInput" defaultValue="Some initial value" />
+      </label>
+      <hr />
+      <label>
+        Checkbox: <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+      </label>
+      <hr />
+      <p>
+        Radio buttons:
+        <label><input type="radio" name="myRadio" value="option1" /> Option 1</label>
+        <label><input type="radio" name="myRadio" value="option2" defaultChecked={true} /> Option 2</label>
+        <label><input type="radio" name="myRadio" value="option3" /> Option 3</label>
+      </p>
+      <hr />
+      <button type="reset">Reset form</button>
+      <button type="submit">Submit form</button>
+    </form>
+  );
 }
-
